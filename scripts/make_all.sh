ae0b37c19e0067835ddaf902178958b091cd87f2
Michael.Imelfort <mike@mikeimelfort.com>
2011-04-12 12:01:04 +1000

    Wrapper to make all the incs at once...

---
+++ scripts/make_all.sh
@@ -0,0 +1,10 @@
+../scripts/pdb_autocode.pl -i air.conf.csv &&
+../scripts/pdb_autocode.pl -i common.conf.csv &&
+../scripts/pdb_autocode.pl -i host_associated_vertebrate.conf.csv &&
+../scripts/pdb_autocode.pl -i host_sample.conf.csv &&
+../scripts/pdb_autocode.pl -i human_associated.conf.csv &&
+../scripts/pdb_autocode.pl -i sediment.conf.csv &&
+../scripts/pdb_autocode.pl -i soil.conf.csv &&
+../scripts/pdb_autocode.pl -i wastewater_sludge.conf.csv &&
+../scripts/pdb_autocode.pl -i water.conf.csv
+
