7c0bffa7519ef03e739f0abb2d43b554510530be
Michael.Imelfort <mike@mikeimelfort.com>
2011-05-13 11:40:10 +1000

    More fixes to make better use of the new tsv files

--- config_files/make_all.sh
+++ config_files/make_all.sh
@@ -1,10 +1,14 @@
-../scripts/pdb_autocode.pl -s -i air.conf.csv &&
-../scripts/pdb_autocode.pl -s -i common.conf.csv &&
-../scripts/pdb_autocode.pl -s -i host_associated_vertebrate.conf.csv &&
-../scripts/pdb_autocode.pl -s -i host_sample.conf.csv &&
-../scripts/pdb_autocode.pl -s -i human_associated.conf.csv &&
-../scripts/pdb_autocode.pl -s -i sediment.conf.csv &&
-../scripts/pdb_autocode.pl -s -i soil.conf.csv &&
-../scripts/pdb_autocode.pl -s -i wastewater_sludge.conf.csv &&
-../scripts/pdb_autocode.pl -s -i water.conf.csv
-
+../scripts/pdb_autocode.pl -s -i air.tsv &&
+../scripts/pdb_autocode.pl -s -i host_associated.tsv &&
+../scripts/pdb_autocode.pl -s -i human_associated.tsv &&
+../scripts/pdb_autocode.pl -s -i human_gut.tsv &&
+../scripts/pdb_autocode.pl -s -i human_oral.tsv &&
+../scripts/pdb_autocode.pl -s -i human_skin.tsv &&
+../scripts/pdb_autocode.pl -s -i human_vaginal.tsv &&
+../scripts/pdb_autocode.pl -s -i mat_biofilm.tsv &&
+../scripts/pdb_autocode.pl -s -i miscellaneous.tsv &&
+../scripts/pdb_autocode.pl -s -i plant_associated.tsv &&
+../scripts/pdb_autocode.pl -s -i sediment.tsv &&
+../scripts/pdb_autocode.pl -s -i soil.tsv &&
+../scripts/pdb_autocode.pl -s -i wastewater_sludge.tsv &&
+../scripts/pdb_autocode.pl -s -i water.tsv
